9f83d27dd0512e58455b47dff138dabf7645edd9
Everzon-Oajaca <eoajacac@miumg.edu.gt>
2025-03-12 14:25:04 -0600

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
   database: 'agenteinteligente', // Nombre de la base de datos
   username: 'agenteinteligente_user', // Usuario de la base de datos
   password: '4zRN1pI4OwyPvX3mjNFHfP4CgQnLc8sm', // Contraseña de la base de datos
-  host: 'dpg-cv7fdb52ng1s7385e2dg-a.oregon-postgres.render.com', // Host del servidor PostgreSQL
+  host: 'dpg-cv7fdb52ng1s7385e2dg-a', // Host del servidor PostgreSQL
   dialect: 'postgres', // El dialecto de la base de datos (PostgreSQL)
   pool: {
     max: 5, // Número máximo de conexiones en el pool
